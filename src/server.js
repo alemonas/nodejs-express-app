9c5b87c70bc27c842b0735a6c333d684a9b7b250
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-25 19:31:41 -0400

    add user and list routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
+
+import userRouter from './resources/user/user.router'
+import listRouter from './resources/list/list.router'
 
 export const app = express()
 const port = 5000
@@ -16,8 +20,15 @@ app.get('/', (req, res) => {
   })
 })
 
+app.use('/api/user', userRouter)
+app.use('api/list', listRouter)
+
 export const start = () => {
   try {
+    mongoose.connect('mongodb://localhost:27017/MyApp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`)
     })
