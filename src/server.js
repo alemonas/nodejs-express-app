de8802181fc68031e06a1992c8b893c3e2d23660
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-25 16:16:21 -0400

    init express app

--- src/server.js
+++ src/server.js
@@ -1,4 +1,27 @@
-import {sum} from './sum'
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 
-console.log(sum(1, 2))
-console.log('server.js')
+export const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'hello',
+  })
+})
+
+export const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
