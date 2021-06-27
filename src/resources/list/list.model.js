549fbb2ee7cf34b0b3c0b42527ffcaf91fdbff50
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-27 16:08:30 -0400

    add timestamp to list model

--- src/resources/list/list.model.js
+++ src/resources/list/list.model.js
@@ -1,19 +1,22 @@
 import mongoose from 'mongoose'
 
-const listSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 50,
+const listSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50,
+    },
+    description: String,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true,
+    },
   },
-  description: String,
-  createdBy: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-})
+  {timestamps: true},
+)
 
 listSchema.index({user: 1, name: 1}, {unique: true})
 
