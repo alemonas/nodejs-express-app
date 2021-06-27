f6f4802beadae5d2bd9ca25e9f880d3548a72a25
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-27 16:07:40 -0400

    add item resources

---
+++ src/resources/item/item.router.js
@@ -0,0 +1,14 @@
+import {Router} from 'express'
+import controllers from '../item/item.controllers'
+
+const router = Router()
+
+router.route('/').get(controllers.getOne).post(controllers.createOne)
+
+router
+  .route('/:id')
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.delete)
+
+export default router
